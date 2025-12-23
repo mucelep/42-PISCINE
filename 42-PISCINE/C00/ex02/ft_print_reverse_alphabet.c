d3707107f2a640924c72cb127c611a17ce139bfe
mucelep <celepm82@gmail.com>
2025-12-23 17:30:50 +0300

    first commit

---
+++ 42-PISCINE/C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mucelep <celepm82@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 06:16:01 by mucelep           #+#    #+#             */
+/*   Updated: 2025/12/23 06:16:01 by mucelep          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
+
+/*int main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
